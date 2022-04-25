79334eb60bdfd4298f64f297f38abd8a02de4e11
Abram Shook <ajshook77@gmail.com>
2022-04-25 17:27:06 -0500

    commit tree b62fc0ba8283236bd327541d034793c4e985c671

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  let hqLocation = 42
+  if (someValue > hqLocation) {
+    return someValue - hqLocation
+  } else {
+    return hqLocation - someValue
+  }
+}
+
+function distanceFromHqInFeet(value2) {
+  const distanceInBlocks = distanceFromHqInBlocks(value2)
+  return distanceInBlocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  //const distanceInFeet = (start + destination) * 264
+  if (start < destination) {
+    return (start - destination) * -1 * 264
+  } else return (start - destination) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  let rideFare = distanceTravelledInFeet(start, destination)
+  if (rideFare > 2500) {
+    return 'cannot travel that far'
+  } else if (rideFare >= 2000) {
+    return 25
+  } else if (rideFare < 400) {
+    return 0
+  } else {
+    return (rideFare - 400) * 0.02
+  }
+}
